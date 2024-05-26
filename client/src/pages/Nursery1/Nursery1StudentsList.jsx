f79624624cd7d453428a381b9fe5ae1abc48391d
One-johnson <113808270+One-johnson@users.noreply.github.com>
2024-05-26 01:43:53 +0000

    done

--- client/src/pages/Nursery1/Nursery1StudentsList.jsx
+++ client/src/pages/Nursery1/Nursery1StudentsList.jsx
@@ -21,29 +21,31 @@ const Nursery1StudentsList = () => {
   return (
     <div>
       <Navbar />
-      <div className="">
-        <div className="relative">
-          <button
-            onClick={toggleFormVisibility}
-            className="fixed top-[110px] right-[60px] bg-violet-800 hover:bg-gray-200 border hover:text-violet-800 text-white py-2 px-2 rounded-full transition duration-300 flex items-center z-10"
-          >
-            <RiAddLine className="text-xl" />
-          </button>
-          {isFormVisible && <AddStudentForm onClose={handleCloseForm} />}
-        </div>
+      <div className="mx-4 sm:mx-8 md:mx-auto md:max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-2xl">
+        <div className="flex justify-between items-center">
+          <div className="relative">
+            <button
+              onClick={toggleFormVisibility}
+              className="fixed top-[110px] right-[60px] bg-violet-800 hover:bg-gray-200 border hover:text-violet-800 text-white py-2 px-2 rounded-full transition duration-300 flex items-center z-10"
+            >
+              <RiAddLine className="text-xl" />
+            </button>
+            {isFormVisible && <AddStudentForm onClose={handleCloseForm} />}
+          </div>
 
-        <div className="fixed top-[110px] font-bold text-2xl bg-violet-700 rounded-lg px-4 py-2 flex items-center text-gray-200 z-10">
-          <Link to="/Creche">
-            <BiArrowBack
-              className="mr-2"
-              style={{ width: "40px", height: "40px" }}
-            />
-          </Link>
-          <h2>NURSERY ONE (1) LEARNERS' LIST</h2>
+          <div className="fixed top-[110px] font-bold text-xl sm:text-2xl bg-violet-700 rounded-lg px-4 py-2 flex items-center text-gray-200 z-10">
+            <Link to="/Creche">
+              <BiArrowBack
+                className="mr-2"
+                style={{ width: "40px", height: "40px" }}
+              />
+            </Link>
+            <h2>NURSERY ONE (1) LEARNERS' LIST</h2>
+          </div>
         </div>
 
         <div className="flex flex-col items-center min-h-screen px-4 py-6 pt-32">
-          <div className="w-full max-w-[1500px] border border-gray-100 rounded-lg mt-16 px-3">
+          <div className="w-full border border-gray-100 rounded-lg mt-16 px-3">
             <StudentTable />
           </div>
         </div>
