1ff6c51f5fe35e01fcc51027179433a81f19236a
One-johnson <113808270+One-johnson@users.noreply.github.com>
2024-05-27 23:32:34 +0000

    update

--- client/src/pages/Nursery1/StudentTable.jsx
+++ client/src/pages/Nursery1/StudentTable.jsx
@@ -1,8 +1,9 @@
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
-import { FaRegEye, FaRegEdit } from "react-icons/fa";
-import { FaFileExcel, FaRegTrashCan, FaFilePdf } from "react-icons/fa6";
+import { FaRegEye, FaRegEdit} from "react-icons/fa";
+import { BsFileEarmarkPdfFill, BsFileEarmarkExcelFill } from "react-icons/bs";
+import { FaFileExcel, FaRegTrashCan,  } from "react-icons/fa6";
 import StatusTag from "../../Components/StatusTag";
 import PropTypes from "prop-types";
 import DataTable from "react-data-table-component";
@@ -305,10 +306,10 @@ const StudentTable = ({ filters, searchText }) => {
           filename={"students.csv"}
           title="Export CSV"
         >
-          <FaFileExcel className="text-green-700 " />
+          <BsFileEarmarkExcelFill className="text-green-700 " />
         </CSVLink>
         <button onClick={exportPDF} title="Export PDF">
-          <FaFilePdf className="text-red-700" />
+          <BsFileEarmarkPdfFill className="text-red-700" />
         </button>
       </div>
       <DataTable
