d3bd2240f38e8a1ca81fda91f93a556b5d78db57
One-johnson <113808270+One-johnson@users.noreply.github.com>
2024-06-10 15:11:39 +0000

    using primereact

--- client/src/pages/Nursery1/Nursery1StudentsList.jsx
+++ client/src/pages/Nursery1/Nursery1StudentsList.jsx
@@ -5,18 +5,10 @@ import { RiAddLine } from "react-icons/ri";
 import { BiArrowBack } from "react-icons/bi";
 import Navbar from "../../Components/Navbar";
 import StudentTable from "./StudentTable";
-import CustomSelect from "../../assets/CustomSelect";
-import { BiSearch } from "react-icons/bi";
 import AddStudentForm from "./AddStudentForm";
 
 const Nursery1StudentsList = () => {
   const [isFormVisible, setIsFormVisible] = useState(false);
-  const [searchText, setSearchText] = useState("");
-  const [filters, setFilters] = useState({
-    age: "",
-    status: "",
-    gender: "",
-  });
 
   const toggleFormVisibility = () => {
     setIsFormVisible(!isFormVisible);
@@ -24,16 +16,6 @@ const Nursery1StudentsList = () => {
 
   const handleCloseForm = () => {
     setIsFormVisible(false);
-  };
-  const handleFilterChange = (e) => {
-    setFilters({
-      ...filters,
-      [e.target.name]: e.target.value,
-    });
-  };
-
-  const handleSearchChange = (e) => {
-    setSearchText(e.target.value);
   };
 
   return (
@@ -65,61 +47,10 @@ const Nursery1StudentsList = () => {
             <h2>NURSERY ONE (1) LEARNERS' LIST</h2>
           </div>
         </div>
-        <div className="mt-[90px] flex items-center justify-between">
-          <div className="filters flex items-center space-x-4 ">
-            <CustomSelect
-              name="age"
-              value={filters.age}
-              onChange={handleFilterChange}
-              options={[
-                { value: "", label: "All Ages" },
-                { value: "1-4", label: "1-4" },
-                { value: "5-9", label: "5-9" },
-                { value: "10-12", label: "10-12" },
-                { value: "13-15", label: "13-15" },
-                { value: "16-18", label: "16-18" },
-                // Add more options as needed
-              ]}
-            />
-            <CustomSelect
-              name="status"
-              value={filters.status}
-              onChange={handleFilterChange}
-              options={[
-                { value: "", label: "All Statuses" },
-                { value: "fresher", label: "fresher" },
-                { value: "continuing", label: "continuing" },
-                { value: "completed", label: "completed" },
-              ]}
-            />
-            <CustomSelect
-              name="gender"
-              value={filters.gender}
-              onChange={handleFilterChange}
-              options={[
-                { value: "", label: "All Genders" },
-                { value: "male", label: "Male" },
-                { value: "female", label: "Female" },
-              ]}
-            />
-          </div>
-          <div className="w-full max-w-[300px] relative">
-            <input
-              type="text"
-              placeholder="Search..."
-              value={searchText}
-              onChange={handleSearchChange}
-              className="pl-10 px-4 py-2 border-2 rounded-2xl w-full max-w-sm mx-auto focus:outline-none focus:border-violet-800 transition duration-300 focus:border-2 hover:border-gray-500 hover:border-2 shadow"
-            />
-            <div className="absolute inset-y-0 left-0 flex items-center pl-4 text-gray-400 pointer-events-none">
-              <BiSearch />
-            </div>
-          </div>
-        </div>
 
-        <div className="pt-4 flex-grow">
-          <div className="w-full border border-gray-100 rounded-lg px-3">
-            <StudentTable filters={filters} searchText={searchText} />
+        <div className="mt-[90px]">
+          <div className="w-full border border-gray-100 rounded-lg px-3 py-3">
+            <StudentTable />
           </div>
         </div>
       </div>
